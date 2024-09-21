92777ef653ede7da57720b0223d9466a8e351a9a
Phuc Nguyen <phucnguyen@Phucs-MacBook-Air.local>
2024-09-21 12:00:02 +0700

    Initial commit

---
+++ src/test/java/com/siuuuuu/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siuuuuu.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
