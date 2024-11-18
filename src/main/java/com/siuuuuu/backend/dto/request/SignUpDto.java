8575e60d532b1f883d7bbe8bd6b870469e44a8a8
Newt Tran <tranngoctan910@gmail.com>
2024-11-18 02:44:39 +0000

    create SignUp

--- src/main/java/com/siuuuuu/backend/dto/request/SignUpDto.java
+++ src/main/java/com/siuuuuu/backend/dto/request/SignUpDto.java
@@ -1,16 +1,28 @@
 package com.siuuuuu.backend.dto.request;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.util.Date;
+
 
 @Data
 @NoArgsConstructor
 public class SignUpDto {
-    @NotBlank(message = "Email không được để trống")
+    @NotEmpty(message = "Họ và tên đệm không được để trống")
+    private String first_name;
+    @NotEmpty(message = "Tên không được để trống")
+    private  String last_name;
+    @NotNull(message = "Ngày sinh không được để trống")
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private Date date_of_birth;
+    @NotEmpty(message = "Email không được để trống")
     @Email(message = "Email không đúng định dạng")
     private String email;
-    @NotBlank(message = "Mật khẩu không được để trống")
+    @NotEmpty(message = "Mật khẩu không được để trống")
     private String password;
+    private  String confirm_password;
 }
