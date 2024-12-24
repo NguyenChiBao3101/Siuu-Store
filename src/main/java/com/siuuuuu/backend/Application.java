8145c6c590a582cef1bcf4a3765b271ee0b53725
Phuc Nguyen <phucnguyen@Phucs-MacBook-Air.local>
2024-12-24 14:58:05 +0700

    Add Jasypt encryptor password configuration to application-gcp.properties

--- src/main/java/com/siuuuuu/backend/Application.java
+++ src/main/java/com/siuuuuu/backend/Application.java
@@ -1,5 +1,6 @@
 package com.siuuuuu.backend;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -7,11 +8,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class Application {
+    @Value("${jasypt.encryptor.password}")
+    private static String JASYPT_ENCRYPTOR_PASSWORD;
+
     public static void main(String[] args) {
-        System.out.println("DB_URL: " + System.getenv("DB_URL"));
-        System.out.println("DB_USERNAME: " + System.getenv("DB_USERNAME"));
-        System.out.println("DB_PASSWORD: " + System.getenv("DB_PASSWORD"));
-        System.out.println("JASYPT_ENCRYPTOR_PASSWORD: " + System.getenv("JASYPT_ENCRYPTOR_PASSWORD"));
+        System.out.println("JASYPT_ENCRYPTOR_PASSWORD: " + JASYPT_ENCRYPTOR_PASSWORD);
         SpringApplication.run(Application.class, args);
     }
 }
