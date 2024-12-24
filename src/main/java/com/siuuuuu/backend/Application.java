4882ce23920c71ca8c9525f1d0feacdf87757cb4
Phuc Nguyen <phucnguyen@Phucs-MacBook-Air.local>
2024-12-24 12:34:50 +0700

    Log database connection details from environment variables in the application startup

--- src/main/java/com/siuuuuu/backend/Application.java
+++ src/main/java/com/siuuuuu/backend/Application.java
@@ -8,6 +8,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class Application {
     public static void main(String[] args) {
+        System.out.println("DB_URL: " + System.getenv("DB_URL"));
+        System.out.println("DB_USERNAME: " + System.getenv("DB_USERNAME"));
+        System.out.println("DB_PASSWORD: " + System.getenv("DB_PASSWORD"));
         SpringApplication.run(Application.class, args);
     }
 }
